3b48ac37568fdfc93fd842d39aad12f7ea0f2c17
PSILVA999 <p.silva0399@gmail.com>
2022-09-14 13:57:32 -0300

    Estrutura do projeto

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Header from './Components/Header'
+import Header from './Components/Block'
+import Generator from './Components/Generator'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <Header/>
+        
+        <Block/>
+        <Generator/>
+      </main>
+    </>
+
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Beloved Block</h1>
+      <span>Create your own authentic container</span>
+    </header>
+  )
+}
+
+export default Header
